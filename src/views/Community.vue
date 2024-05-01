ab7883a4c9b9f8b33484bab1567dd60abfc82872
Taha Dostifam <mr.tahadostifam@gmail.com>
2024-05-01 12:37:38 +0330

    feat: community page completed

--- src/views/Community.vue
+++ src/views/Community.vue
@@ -1,38 +1,113 @@
 <template>
-    <div class="flexList anim--fadeIn">
-        <div class="flexGrid flexGrid--2">
-            <div class="flexGrid-item card card--plain card--no-padding">
-                <iframe src="https://discord.com/widget?id=1023243680829681704&theme=dark" style="width: 100%" height="525"
-                    allowtransparency="true" frameborder="0"
-                    sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+    <div id="community" class="flexList anim--fadeIn">
+        <div class="card card--full youtube">
+            <div class="card-video-grid">
+                <div>
+                    <div class="card-header">
+                        <h3>
+                            <span class="fa fa-brands fa-youtube"></span>
+                            Youtube
+                        </h3>
+                    </div>
+                    <div class="card-content">
+                        <div class="text text--rich">
+                            <p>We're always uploading new videos, so be sure to subscribe and hit the notification bell
+                                to
+                                stay up-to-date.
+                                <br />
+                                <br />
+                                Don't forget to leave us a comment and let us know what you think of our videos!
+                            </p>
+                        </div>
+                        <a class="btn btn--primary" href="https://google.com" target="_blank">
+                            <span>Subscribe on youtube!</span>
+                            <span class="mdi material-icons">arrow_forward</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="card-video">
+                    <iframe src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1"></iframe>
+                </div>
             </div>
-            <div class="card">
+        </div>
+
+        <div class="flexGrid flexGrid--3">
+            <a href="https://google.com" class="card discord">
                 <div class="card-header">
                     <h3>
                         <span class="fa fa-brands fa-discord"></span>
-                        Join Discord
+                        Discord
                     </h3>
                 </div>
                 <div class="card-content">
                     <div class="text text--rich">
-                        <p>We have both a Discord server and Forums but our community is on Discord, a cross-platform voice
-                            and text chat platform designed for community of all sizes.</p>
-                        <p>Join us to chat with other users and developers, participate in our community events, or find out
-                            the latest news before they are published! 👀.</p>
+                        <p>Beyond discussions, our Discord is the place to hang out, have fun, and connect with others.
+                            This platform, with its high-quality voice and text features, fosters a vibrant space for a
+                            thriving community.</p>
                     </div>
                 </div>
-                <div class="card-footer">
-                    <a class="btn btn--link btn--inline btn--med" href="https://discord.gg/vanilla-os-1023243680829681704"
-                        target="_blank">
-                        <span>Join Discord</span>
-                        <span class="mdi material-icons">arrow_forward</span>
-                    </a>
+            </a>
+            <a href="https://google.com" class="card mostodon">
+                <div class="card-header">
+                    <h3>
+                        <span class="fa fa-brands fa-mastodon"></span>
+                        Mostodon
+                    </h3>
                 </div>
+                <div class="card-content">
+                    <div class="text text--rich">
+                        <p>We're always uploading new videos, so be sure to subscribe and hit the notification bell to
+                            stay up-to-date.
+                            <br />
+                            <br />
+                            Don't forget to leave us a comment and let us know what you think of our videos!
+                        </p>
+                    </div>
+                </div>
+            </a>
+            <a href="https://google.com" class="card telegram">
+                <div class="card-header">
+                    <h3>
+                        <span class="fa fa-brands fa-telegram"></span>
+                        Telegram
+                    </h3>
+                </div>
+                <div class="card-content">
+                    <div class="text text--rich">
+                        <p>We're always uploading new videos, so be sure to subscribe and hit the notification bell to
+                            stay up-to-date.
+                            <br />
+                            <br />
+                            Don't forget to leave us a comment and let us know what you think of our videos!
+                        </p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="card card--full matrix">
+            <div class="card-header">
+                <h3>
+                    Matrix
+                </h3>
+            </div>
+            <div class="card-content">
+                <div class="text text--rich">
+                    <p><b>Tired of relying on centralized platforms?</b> Matrix.to offers a revolutionary way to connect. It's a decentralized communication app, putting you in control of your data and interactions.
+                        <br />
+                        <br />
+                        Join the future of communication. Sign up for Matrix.to today!
+                    </p>
+                </div>
+                <a class="btn btn--primary" href="https://matrix.to/#/%23parch:matrix.org" target="_blank">
+                    <span>Go to matrix.to!</span>
+                    <span class="mdi material-icons">arrow_forward</span>
+                </a>
             </div>
         </div>
     </div>
 </template>
-  
+
 <script lang="ts">
 import { defineComponent } from 'vue';
 
@@ -40,4 +115,3 @@ export default defineComponent({
     name: 'community',
 });
 </script>
-  
