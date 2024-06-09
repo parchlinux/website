6d0cf28fa8cd287dc0990587988ccdfa2aee66c5
hikari <94219809+metantesan@users.noreply.github.com>
2024-06-09 13:28:17 +0330

    Chenge Mirror Url path

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -13,7 +13,7 @@
                             </p>
                         </div>
                         <div class="spacer"></div>
-                        <a href="http://mirror.parchlinux.com/plasma/Parchlinux-Plasma-latest.iso" class="btn btn--primary">
+                        <a href="http://mirror.parchlinux.com/plasma/" class="btn btn--primary">
                             <span class="mdi material-icons-outlined">save_alt</span>
                             <span>Download Now</span>
                         </a>
@@ -39,7 +39,7 @@
                             </p>
                         </div>
                         <div class="spacer"></div>
-                        <a href="http://mirror.parchlinux.com/gnome/Parchlinux-Gnome-latest.iso" target="_blank"
+                        <a href="http://mirror.parchlinux.com/gnome/" target="_blank"
                             class="btn btn--primary">
                             <span class="mdi material-icons">save_alt</span>
                             <span>Download Now</span>
