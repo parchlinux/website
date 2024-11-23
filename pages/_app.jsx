4b8a9481cd8c0ceb023bc3b516e81cbd4439dbfd
ashkano <username_subject_to_change@protonmail.com>
2024-11-23 22:45:26 +0330

    favicon

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,18 @@
 import BaseLayout from "@/components/Base";
 import "@/styles/globals.css";
+import { useTranslation } from "@/utils/translation";
 import { ThemeProvider } from "next-themes";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
+  const { t } = useTranslation()
+
   return <ThemeProvider attribute="class">
     <BaseLayout>
       <Component {...pageProps} />
+      <Head>
+        <title>{t('home.title')}</title>
+      </Head>
     </BaseLayout>
   </ThemeProvider>;
 }
