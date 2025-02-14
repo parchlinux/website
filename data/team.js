31e0f4926f3f6cee8d90546a5612e61d9d6cb511
ERFouX <ERFouX@gmail.com>
2025-02-14 17:39:43 +0330

    Update teamData: Added new members and updated roles

--- data/team.js
+++ data/team.js
@@ -1,221 +1,237 @@
 const teamData = [
-  {
-    "name": {
-      en: "Sohrab Behdani",
-      fa: "سهراب بهدانی"
-    },
-    "stack": {
-      en: "Founder",
-      fa: "موسس"
-    },
-    "image": "https://avatars.githubusercontent.com/behdanisohrab",
-    "socials": {
-      "twitter": "https://twitter.com/sbehdani",
-      "github": "https://github.com/behdanisohrab",
-      "mastodon": "https://persadon.com/@sohrab",
-      "donate": "https://daramet.com/sohrabbehdani"
-    }
-  },
-  {
-    "name": {
-      en: "Parsa",
-      fa: "پارسا"
-    },
-    "stack": {
-      en: "Founder",
-      fa: "موسس"
-    },
-    "image": "https://avatars.githubusercontent.com/metantesan",
-    "socials": {
-      "github": "https://github.com/metantesan",
-      "mastodon": "https://mastodon.bsd.cafe/@hikari",
-      "donate": "https://daramet.com/hikari",
-      "twitter": "https://twitter.com/metantesan"
-    }
-  },
-  {
-    "name": {
-      en: "Hamid Reza",
-      fa: "حمیدرضا"
-    },
-    "stack": {
-      en: "Maintainer",
-      fa: "نگهدارنده"
-    },
-    "image": "https://avatars.githubusercontent.com/HRMCA",
-    "socials": {
-      "github": "https://github.com/HRMCA",
-      "mastodon": "https://mas.to/@HRM_CA"
-    }
-  },
-  {
-    "name": {
-      en: "Erfan Arabi",
-      fa: "عرفان عربی"
-    },
-    "stack": {
-      en: "Team Leader",
-      fa: "راهبر تیم"
-    },
-    "image": "https://avatars.githubusercontent.com/ERFouX",
-    "socials": {
-      "github": "https://github.com/ERFouX",
-      "mastodon": "https://mas.to/@ERFouX",
-      "donate": "https://daramet.com/ERFouX"
-    }
-  },
-  {
-    "name": {
-      en: "Pouya Madankar",
-      fa: "پویا مدنکار"
-    },
-    "stack": {
-      en: "Frontend Developer",
-      fa: "توسعه‌دهنده فرانت‌اند"
-    },
-    "image": "https://avatars.githubusercontent.com/pouyaMK",
-    "socials": {
-      "github": "https://github.com/pouyaMK"
-    }
-  },
-  {
-    "name": {
-      en: "AhmadReza Khatamian",
-      fa: "احمدرضا خاتمیان"
-    },
-    "stack": {
-      en: "Graphic Designer",
-      fa: "طراح گرافیک"
-    },
-    "image": "https://avatars.githubusercontent.com/ahmadreza3",
-    "socials": {
-      "github": "https://github.com/ahmadreza3",
-      "mastodon": "https://mas.to/@ahmad00000"
-    }
-  },
-  {
-    "name": {
-      en: "Pourya Barkati",
-      fa: "پوریا برکتی"
-    },
-    "stack": {
-      en: "Rust Developer",
-      fa: "توسعه‌دهنده راست"
-    },
-    "image": "https://avatars.githubusercontent.com/BrPrS",
-    "socials": {
-      "github": "https://github.com/BrPrS",
-      "donate": "https://daramet.com/prbarkati"
-    }
-  },
-  {
-    "name": {
-      en: "Ali Mohammad Esmaeeli",
-      fa: "علی محمد اسماعیلی"
-    },
-    "stack": {
-      en: "Frontend Developer",
-      fa: "توسعه‌دهنده فرانت‌اند"
-    },
-    "image": "https://avatars.githubusercontent.com/AliMohammad-Esmaeeli",
-    "socials": {
-      "github": "https://github.com/AliMohammad-Esmaeeli",
-      "twitter": "https://twitter.com/AliMoha03254987"
-    }
-  },
-  {
-    "name": {
-      en: "Mostafa Aminzar",
-      fa: "مصطفی امین‌زار"
-    },
-    "stack": {
-      en: "UI/UX Designer",
-      fa: "طراح UI/UX"
-    },
-    "image": "https://avatars.githubusercontent.com/MosiO1998",
-    "socials": {
-      "github": "https://github.com/MosiO1998",
-      "donate": "https://daramet.com/mosio"
-    }
-  },
-  {
-    "name": {
-      en: "Meshya",
-      fa: "مشیا"
-    },
-    "stack": {
-      en: "Contributer",
-      fa: "مشارکت کننده"
-    },
-    "image": "https://avatars.githubusercontent.com/meshya",
-    "socials": {
-      "github": "https://github.com/meshya",
-      "mastodon": "https://mas.to/@meshya",
-      "donate": "https://daramet.com/meshya"
-    }
-  },
-  {
-    "name": {
-      en: "Ali Almasi",
-      fa: "علی الماسی"
-    },
-    "stack": {
-      en: "Web Developer",
-      fa: "توسعه‌دهنده وب"
-    },
-    "image": "https://avatars.githubusercontent.com/AliAlmasi",
-    "socials": {
-      "github": "https://github.com/AliAlmasi",
-      "donate": "https://daramet.com/alialmasi",
-      "mastodon": "https://ieji.de/@almasi"
-    }
-  },
-  {
-    "name": {
-      en: "Ali Lotfi",
-      fa: "علی لطفی"
-    },
-    "stack": {
-      en: "Graphic Designer",
-      fa: "طراح گرافیک"
-    },
-    "image": "https://avatars.githubusercontent.com/AliLotfi09",
-    "socials": {
-      "github": "https://github.com/AliLotfi09",
-      "donate": "https://daramet.com/Im_Lotfi"
-    }
-  },
-  {
-    "name": {
-      en: "Mobin Firooz",
-      fa: "مبین فیروز"
-    },
-    "stack": {
-      en: "ّFrontend Developer",
-      fa: "توسعه‌دهنده فرانت‌اند"
-    },
-    "image": "https://avatars.githubusercontent.com/Bravemobin",
-    "socials": {
-      "github": "https://github.com/Bravemobin/",
-      "twitter": "https://x.com/Bravemobin/",
-      "donate": "https://daramet.com/bravemobin/"
-    }
-  },
-  {
-    "name": {
-      en: "Mahdi Gholamhosseini",
-      fa: "مهدی غلام‌حسینی"
-    },
-    "stack": {
-      en: "Web Developer",
-      fa: "توسعه‌دهنده وب"
-    },
-    "image": "https://avatars.githubusercontent.com/m6hdix",
-    "socials": {
-      "github": "https://github.com/m6hdix",
-      "donate": "https://daramet.com/m6hdix"
-    }
-  }
-]
-
-export default teamData;
+	{
+		"name": {
+			en: "Sohrab Behdani",
+			fa: "سهراب بهدانی"
+		},
+		"stack": {
+			en: "Founder",
+			fa: "موسس"
+		},
+		"image": "https://avatars.githubusercontent.com/behdanisohrab",
+		"socials": {
+			"twitter": "https://twitter.com/sbehdani",
+			"github": "https://github.com/behdanisohrab",
+			"mastodon": "https://persadon.com/@sohrab",
+			"donate": "https://daramet.com/sohrabbehdani"
+		}
+	},
+	{
+		"name": {
+			en: "Parsa",
+			fa: "پارسا"
+		},
+		"stack": {
+			en: "Founder",
+			fa: "موسس"
+		},
+		"image": "https://avatars.githubusercontent.com/metantesan",
+		"socials": {
+			"github": "https://github.com/metantesan",
+			"mastodon": "https://mastodon.bsd.cafe/@hikari",
+			"donate": "https://daramet.com/hikari",
+			"twitter": "https://twitter.com/metantesan"
+		}
+	},
+	{
+		"name": {
+			en: "Hamid Reza",
+			fa: "حمیدرضا"
+		},
+		"stack": {
+			en: "Maintainer",
+			fa: "نگهدارنده"
+		},
+		"image": "https://avatars.githubusercontent.com/HRMCA",
+		"socials": {
+			"github": "https://github.com/HRMCA",
+			"mastodon": "https://mas.to/@HRM_CA"
+		}
+	},
+	{
+		"name": {
+			en: "Erfan Arabi",
+			fa: "عرفان عربی"
+		},
+		"stack": {
+			en: "Team Leader",
+			fa: "راهبر تیم"
+		},
+		"image": "https://avatars.githubusercontent.com/ERFouX",
+		"socials": {
+			"github": "https://github.com/ERFouX",
+			"mastodon": "https://mas.to/@ERFouX",
+			"donate": "https://daramet.com/ERFouX"
+		}
+	},
+	{
+		"name": {
+			en: "Mobin Firooz",
+			fa: "مبین فیروز"
+		},
+		"stack": {
+			en: "ّFrontend Developer",
+			fa: "توسعه‌دهنده فرانت‌اند"
+		},
+		"image": "https://avatars.githubusercontent.com/Bravemobin",
+		"socials": {
+			"github": "https://github.com/Bravemobin/",
+			"twitter": "https://x.com/Bravemobin/",
+			"donate": "https://daramet.com/bravemobin/"
+		}
+	},
+	{
+		"name": {
+			en: "AhmadReza Khatamian",
+			fa: "احمدرضا خاتمیان"
+		},
+		"stack": {
+			en: "Graphic Designer",
+			fa: "طراح گرافیک"
+		},
+		"image": "https://avatars.githubusercontent.com/ahmadreza3",
+		"socials": {
+			"github": "https://github.com/ahmadreza3",
+			"mastodon": "https://mas.to/@ahmad00000"
+		}
+	},
+	{
+		"name": {
+			en: "Gnkalk",
+			fa: "جنگالک"
+		},
+		"stack": {
+			en: "Gitee Developer",
+			fa: "توسعه‌دهنده گیتی"
+		},
+		"image": "https://avatars.githubusercontent.com/gnkalk",
+		"socials": {
+			"github": "https://github.com/gnkalk",
+			"mastodon": "https://toot.community/@gnkalk",
+		}
+	},
+	{
+		"name": {
+			en: "Pouya Madankar",
+			fa: "پویا مدنکار"
+		},
+		"stack": {
+			en: "Frontend Developer",
+			fa: "توسعه‌دهنده فرانت‌اند"
+		},
+		"image": "https://avatars.githubusercontent.com/pouyaMK",
+		"socials": {
+			"github": "https://github.com/pouyaMK"
+		}
+	},
+	{
+		"name": {
+			en: "Mostafa Aminzar",
+			fa: "مصطفی امین‌زار"
+		},
+		"stack": {
+			en: "UI/UX Designer",
+			fa: "طراح UI/UX"
+		},
+		"image": "https://avatars.githubusercontent.com/MosiO1998",
+		"socials": {
+			"github": "https://github.com/MosiO1998",
+			"donate": "https://daramet.com/mosio"
+		}
+	},
+	{
+		"name": {
+			en: "Meshya",
+			fa: "مشیا"
+		},
+		"stack": {
+			en: "Contributer",
+			fa: "مشارکت کننده"
+		},
+		"image": "https://avatars.githubusercontent.com/meshya",
+		"socials": {
+			"github": "https://github.com/meshya",
+			"mastodon": "https://mas.to/@meshya",
+			"donate": "https://daramet.com/meshya"
+		}
+	},
+	{
+		"name": {
+			en: "Ali Almasi",
+			fa: "علی الماسی"
+		},
+		"stack": {
+			en: "Web Developer",
+			fa: "توسعه‌دهنده وب"
+		},
+		"image": "https://avatars.githubusercontent.com/AliAlmasi",
+		"socials": {
+			"github": "https://github.com/AliAlmasi",
+			"donate": "https://daramet.com/alialmasi",
+			"mastodon": "https://ieji.de/@almasi"
+		}
+	},
+	{
+		"name": {
+			en: "Ali Lotfi",
+			fa: "علی لطفی"
+		},
+		"stack": {
+			en: "Graphic Designer",
+			fa: "طراح گرافیک"
+		},
+		"image": "https://avatars.githubusercontent.com/AliLotfi09",
+		"socials": {
+			"github": "https://github.com/AliLotfi09",
+			"donate": "https://daramet.com/Im_Lotfi"
+		}
+	},
+	{
+		"name": {
+			en: "Ali Mohammad Esmaeeli",
+			fa: "علی محمد اسماعیلی"
+		},
+		"stack": {
+			en: "Frontend Developer",
+			fa: "توسعه‌دهنده فرانت‌اند"
+		},
+		"image": "https://avatars.githubusercontent.com/AliMohammad-Esmaeeli",
+		"socials": {
+			"github": "https://github.com/AliMohammad-Esmaeeli",
+			"twitter": "https://twitter.com/AliMoha03254987"
+		}
+	},
+	{
+		"name": {
+			en: "Mahdi Gholamhosseini",
+			fa: "مهدی غلام‌حسینی"
+		},
+		"stack": {
+			en: "Web Developer",
+			fa: "توسعه‌دهنده وب"
+		},
+		"image": "https://avatars.githubusercontent.com/m6hdix",
+		"socials": {
+			"github": "https://github.com/m6hdix",
+			"donate": "https://daramet.com/m6hdix"
+		}
+	},
+	{
+		"name": {
+			en: "Ilia Asadi",
+			fa: "ایلیا اسدی"
+		},
+		"stack": {
+			en: "Graphic Designer",
+			fa: "گرافیک دیزاینر"
+		},
+		"image": "https://avatars.githubusercontent.com/iliageek",
+		"socials": {
+			"github": "https://github.com/iliageek",
+			"mastodon": "https://mastodon.social/@iliageek1",
+			"donate": ""
+		}
+	}
+	]
+	
+	export default teamData;
